6532728f6fbbd9d1f97270cb11a46d52656cdce7
SEONGSIC LEE <dpdms7642@naver.com>
2025-10-27 15:28:10 +0900

    fix: recommend_module.py

--- project_root1/recommend_module.py
+++ project_root1/recommend_module.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import numpy as np
+import yaml
 from typing import Dict, List, Tuple
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
